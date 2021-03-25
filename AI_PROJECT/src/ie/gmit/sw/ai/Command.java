7644b60e135c73910ba1bd8de9fe97c9bc984133
Keith Nolan <g00351932@gmit.ie>
2021-03-25 15:51:37 +0000

    Starting project initital commit, stub files

---
+++ AI_PROJECT/src/ie/gmit/sw/ai/Command.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw.ai;
+
+/*
+ * Use implementations of this functional interface to specify
+ * how a computer controlled game character should behave.
+ */
+
+@FunctionalInterface
+public interface Command {
+
+	public void execute();
+}
